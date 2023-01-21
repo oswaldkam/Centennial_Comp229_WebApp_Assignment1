428dea5eace96a3f098b270c6c38a8faee428d10
Oswald <oswaldkam@gmail.com>
2023-01-21 01:58:33 -0500

    Add data json for template

---
+++ Portfilio/data/projectData.js
@@ -0,0 +1,25 @@
+module.exports = {
+  projectData: [
+    {
+      title: "Blind Dog Agent",
+      role: "Principle Developer",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra, arcu ac gravida finibus, augue nibh hendrerit tellus, eu pretium sapien mauris eu nisi. Mauris vel odio vitae turpis convallis aliquam. Curabitur ac fermentum sem. Quisque at arcu enim. Nulla libero dolor, faucibus sed metus ut, pulvinar suscipit quam. Vestibulum sodales nunc nec ante viverra, nec rhoncus augue consectetur. Donec nec ullamcorper risus. Donec porttitor velit a turpis maximus rutrum at vel erat. Pellentesque vitae leo non nunc fermentum volutpat. Donec facilisis, dui quis placerat porta, quam arcu dignissim lacus, tincidunt sagittis massa ante nec sem. Pellentesque et facilisis ex.",
+      path: "images/projects/pythonAI.png",
+    },
+    {
+      title: "Singer",
+      role: "System Designer, Principle Developer, Scrum Master",
+      description:
+        "Integer interdum ut arcu sit amet cursus. Phasellus sed nunc a ante elementum tincidunt non ut massa. Nullam sollicitudin urna felis, porta dictum sapien vulputate congue. Aliquam laoreet quis turpis dapibus blandit. Vestibulum porttitor euismod tellus eu convallis. Nam quam tortor, accumsan sed pretium pretium, ullamcorper sit amet dui. Duis accumsan consequat cursus. Maecenas vel iaculis diam. Phasellus id tincidunt libero. Praesent mollis, lorem sit amet pharetra vestibulum, ante tortor mollis arcu, aliquam consectetur nisl turpis quis sem. Morbi eleifend condimentum ante eget tincidunt. Vivamus gravida, dolor et ultrices volutpat, mi enim hendrerit tellus, sed rhoncus enim nisl in mauris. Curabitur pretium sodales dolor vel ultricies.",
+      path: "images/projects/singer.png",
+    },
+    {
+      title: "Protfolio",
+      role: "System Designer, Principle Full-Stack Developer",
+      description:
+        "Phasellus varius faucibus ligula, a efficitur lectus hendrerit ultrices. Phasellus consectetur fringilla sagittis. Fusce vel ornare tellus, quis pharetra est. Maecenas elementum odio dolor, quis gravida nisi molestie quis. Pellentesque sit amet augue sit amet arcu aliquam sodales. Ut in dapibus dolor. Aliquam eros lectus, vestibulum et ligula sed, maximus bibendum dui. Suspendisse fringilla vel turpis ut molestie. Morbi pretium est vestibulum sapien faucibus sodales. Aenean quis suscipit risus. Quisque ullamcorper vestibulum erat, sit amet convallis ante laoreet quis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas sagittis commodo odio a cursus.",
+      path: "images/projects/protfolio.png",
+    },
+  ],
+};
---
+++ Portfilio/data/servicesData.js
@@ -0,0 +1,12 @@
+module.exports = {
+  services: [
+    "System design",
+    "C# consulation",
+    "iOS appdevelop",
+    "Donec elementum purus",
+    "vel condimentum laoreet",
+    "In a mattis",
+    "tellus",
+    "Sed consectetur",
+  ],
+};
