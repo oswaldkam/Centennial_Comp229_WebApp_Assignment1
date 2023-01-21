2b709e126f0ae2ac532c5892211022a946cf88e1
Oswald <oswaldkam@gmail.com>
2023-01-21 01:12:35 -0500

    Update router

--- Portfilio/routes/index.js
+++ Portfilio/routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var projectData = require("../data/projectData");
+var servicesData = require("../data/servicesData");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("basePage", { title: "Home", page: "home", data: null });
+});
+router.get("/aboutMe", function (req, res, next) {
+  res.render("basePage", { title: "About Me", page: "aboutMe", data: null });
+});
+router.get("/services", function (req, res, next) {
+  res.render("basePage", {
+    title: "Services",
+    page: "services",
+    data: servicesData.services,
+  });
+});
+router.get("/projects", function (req, res, next) {
+  res.render("basePage", {
+    title: "Project",
+    page: "projects",
+    data: projectData.projectData,
+  });
+});
+router.get("/contact", function (req, res, next) {
+  res.render("basePage", { title: "Contact", page: "contact", data: null });
 });
 
 module.exports = router;
